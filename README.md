5662a559851bcacb94ad115838a6fb818d01b834
Real-Khan <95843555+Real-Khan@users.noreply.github.com>
2023-04-10 13:25:02 +0000

    Adding more user experience and readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Web_App_Assignment_STEAM_VN
+# User Profile Web Application
+
+This web application allows users to view profile pages with posts, comments, and photo albums. It's built using Node.js, Express, and EJS, and powered by the JSONPlaceholder API.
+
+## Features
+
+- User profile pages with basic information
+- Display user's posts and comments
+- Display user's photo albums with thumbnails
+- Navigation bar
+- Search feature to find users by name or username
+- Responsive design and layout
+
+## Getting Started
+
+### Prerequisites
+
+To run this project, you'll need:
+
+- [Node.js](https://nodejs.org/) (version 14.x or higher)
+- [npm](https://www.npmjs.com/) (usually installed with Node.js)
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/realkhanhoang/Web_App_Assignment_Steam_VN.git
+```
+
+2. Navigate to the project folder
+```bash
+cd Web_App_Assignment_Steam_VN
+```
+
+3. Install the project dependencies
+```bash
+npm install
+```
+
+## Running the application
+To start the application in development mode
+```bash
+npm start
+```
+Open your web browser and visit http://localhost:3000 to view the application.
+
+## Built With
+- NodeJS
+- Express
+- EJS
+- JSONPlaceholder API
+
--- app.js
+++ app.js
@@ -7,40 +7,39 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
-    const usersResponse = await axios.get('https://jsonplaceholder.typicode.com/users');
-    const users = usersResponse.data;
-    res.render('index', { users });
-  });
-  
+	const usersResponse = await axios.get('https://jsonplaceholder.typicode.com/users');
+	const users = usersResponse.data;
+	res.render('index', { users });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
 
 app.get('/profile/:id', async (req, res) => {
-    const userId = req.params.id;
-    // Fetch the user data
-    const userResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+	const userId = req.params.id;
+	// Fetch the user data
+	const userResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
 
-    // Fetch the posts for the user
-    const postsResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+	// Fetch the posts for the user
+	const postsResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
 
-    // Fetch the albums for the user
-    const albumsResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
+	// Fetch the albums for the user
+	const albumsResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
 
-    const user = userResponse.data;
-    const posts = postsResponse.data;
-    const albums = albumsResponse.data;
+	const user = userResponse.data;
+	const posts = postsResponse.data;
+	const albums = albumsResponse.data;
 
-    // Fetch the photos for each album  
-    const albumIds = albums.map(album => album.id);
-    const photosPromises = albumIds.map(albumId => axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`));
-    const photosResponses = await Promise.all(photosPromises);
-  
-    for (let i = 0; i < albums.length; i++) {
-      albums[i].photos = photosResponses[i].data;
-    }
-    res.render('profile', { user, posts, albums });
-  });
+	// Fetch the photos for each album  
+	const albumIds = albums.map(album => album.id);
+	const photosPromises = albumIds.map(albumId => axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`));
+	const photosResponses = await Promise.all(photosPromises);
 
-  
+	for (let i = 0; i < albums.length; i++) {
+		albums[i].photos = photosResponses[i].data;
+	}
+	res.render('profile', { user, posts, albums });
+});
+
